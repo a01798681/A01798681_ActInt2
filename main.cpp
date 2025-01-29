a73bec53f73a2b8e1074eee817e077b5587689c2
Andrés Cabrera <119537317+a01798681@users.noreply.github.com>
2025-01-29 00:00:01 -0600

    NAMEEEEES

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ public:
     }
 };
 
-vector<Edge> getEdges(int N, const vector<vector<int>> &graph) {
+vector<Edge> GetEdges(int N, const vector<vector<int>> &graph) {
     vector<Edge> edges;
     for (int i = 0; i < N; ++i)
         for (int j = i + 1; j < N; ++j)
@@ -61,7 +61,7 @@ vector<Edge> getEdges(int N, const vector<vector<int>> &graph) {
 }
 
 vector<Edge> kruskal_mst(int N, const vector<vector<int>> &graph) {
-    vector<Edge> edges = getEdges(N, graph);
+    vector<Edge> edges = GetEdges(N, graph);
     sort(edges.begin(), edges.end());
     
     UnionFind uf(N);
